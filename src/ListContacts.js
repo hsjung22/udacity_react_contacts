d76480112be06482a403cd4c4ce196da0c72e455
hsjung22 <hsjung22@gmail.com>
2017-07-06 20:42:50 -0700

    feat: Add show all functionality

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
 
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
+
 
   render() {
     const { contacts, onDeleteContact } = this.props
@@ -43,6 +47,14 @@ class ListContacts extends Component {
             onChange={(e) => this.updateQuery(e.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className="contact-list">
           {showingContacts.map((contact) =>
             <li key={contact.id} className="contact-list-item">
