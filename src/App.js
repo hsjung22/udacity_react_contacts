2a9b7ffa9a0feacfc67ef10ea271f376fb8f093a
hsjung22 <hsjung22@gmail.com>
2017-07-08 15:41:00 -0700

    Add CreateContact component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
+    screen: 'list', //'list', 'create'
     contacts: []
   }
   componentDidMount() {
@@ -21,13 +23,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        {this.state.screen === 'list' && (
+          <ListContacts 
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
       </div>
     )
   }
 }
 
-export default App;
+export default App
